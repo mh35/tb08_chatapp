6b9f52737524151b3553317243a0b87d96c3bb2e
mh35 <mh35jp@gmail.com>
2020-02-17 13:27:11 +0900

    Define deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-which zip
+set -eu
 
 date_prfx=`date '+%Y%m%d%H%M%S'`
 
@@ -9,7 +9,9 @@ cd api
 mkdir dst/code
 cd code
 for dname in *; do
+  set +e
   if [ -d $dname ]; then
+    set -e
     cd $dname
     zip -r ../../dst/code/${date_prfx}_${dname}.zip .
     cd ..
@@ -17,4 +19,25 @@ for dname in *; do
 done
 cd ..
 sed -e "s/Key: code\//Key: code\/${date_prfx}_/g" formation.yml >\
-dst/formation.yml
+dst/formation_${date_prfx}.yml
+cd dst
+aws s3 sync . s3://${BUCKET_NAME}
+set +e
+aws cloudformation stack-exists --stack-name $STACK_NAME
+if [ $? = 0 ]; then
+  set -e
+  aws cloudformation update-stack --stack-name $STACK_NAME \
+  --template-url s3://${BUCKET_NAME}/formation_${date_prfx}.yml \
+  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+  --parameters ParameterKey=codeBucketName,ParameterValue=$BUCKET_NAME
+  aws cloudformation wait stack-update-complete --stack-name \
+  $STACK_NAME
+else
+  set -e
+  aws cloudformation create-stack --stack-name $STACK_NAME \
+  --template-url s3://${BUCKET_NAME}/formation_${date_prfx}.yml \
+  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+  --parameters ParameterKey=codeBucketName,ParameterValue=$BUCKET_NAME
+  aws cloudformation wait stack-create-complete --stack-name \
+  $STACK_NAME
+fi
