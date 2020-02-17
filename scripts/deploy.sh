1b65d1ac595ea6f2d860868773ab9f6d843b4f06
mh35 <mh35jp@gmail.com>
2020-02-17 09:15:53 +0900

    Implementing action script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 which zip
+
+date_prfx=`date '+%Y%m%d%H%M%S'`
+
+cd api
+￼mkdir dst
+mkdir dst/code
+cd code
+for dname in *; do
+  if [ -d $dname ]; then
+    cd $dname
+    zip -r ../../dst/code/${date_prfx}_${dname}.zip .
+    cd ..
+  fi
+done
+cd ..
+sed -e "s/Key: code\//Key: code\/${date_prfx}_/g" formation.yml >\
+dst/formation.yml
